43f63435cf396762dac513ffa191544888495963
= <manni.basi@gmail.com>
2026-01-14 15:26:04 +0000

    Simple change to get back to green

--- src/palindrome.test.ts
+++ src/palindrome.test.ts
@@ -1,3 +1,5 @@
+import { isPalindrome } from "./palindromeChecker";
+
 describe("Palindrome Tests", () => {
   it("Should return true for a valid palindrome", () => {
     expect(isPalindrome("wow")).toBe(true);
