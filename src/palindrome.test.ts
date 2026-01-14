97a190a00deba9766fb11363a7f00471c7edfffb
= <manni.basi@gmail.com>
2026-01-14 22:09:51 +0000

    Added other valid Palindrome test cases

--- src/palindrome.test.ts
+++ src/palindrome.test.ts
@@ -1,10 +1,17 @@
 import { isPalindrome } from "./palindromeChecker";
 
 describe("Palindrome Tests", () => {
-  it.each(["wow", "lol", "pop", "anna", "anna!", "pop.", "lol;"])(
-    "Should return true for valid palindrome input: %s",
-    (input: string) => {
-      expect(isPalindrome(input)).toBe(true);
-    }
-  );
+  it.each([
+    "wow",
+    "lol",
+    "pop",
+    "anna",
+    "anna!",
+    "pop.",
+    "lol;",
+    "A man, a plan, a canal, Panama!",
+    "Race car"
+  ])("Should return true for valid palindrome input: %s", (input: string) => {
+    expect(isPalindrome(input)).toBe(true);
+  });
 });
