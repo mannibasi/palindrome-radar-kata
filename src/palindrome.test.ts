1de690a1e79f9fed137cac5438d65d70bb99662f
= <manni.basi@gmail.com>
2026-01-14 21:53:21 +0000

    Added failing test for input with punctuation

--- src/palindrome.test.ts
+++ src/palindrome.test.ts
@@ -1,7 +1,7 @@
 import { isPalindrome } from "./palindromeChecker";
 
 describe("Palindrome Tests", () => {
-  it.each(["wow", "lol", "pop"])(
+  it.each(["wow", "lol", "pop", "anna", "anna!"])(
     "Should return true for valid palindrome input: %s",
     (input: string) => {
       expect(isPalindrome(input)).toBe(true);
